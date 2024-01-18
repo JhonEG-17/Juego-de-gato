97dd093fd98c74ef7c42e95755d4466cac776d66
JhonEG-17 <jhonataneg@outlook.com>
2024-01-18 10:29:54 -0600

    cree funcion tipo void para construir el tablero y la agregue al main()

--- Program.cpp
+++ Program.cpp
@@ -1,1 +1,29 @@
-ECHO est  activado.
+#include<iostream>
+
+using namespace std;
+
+void construirTablero() {
+    
+    for(int i = 0; i < 3; i++) {
+        //el primer for es para el eje "x"
+
+        for(int j = 0; j < 3; j++) {
+            //este segundo es para el eje "y"
+
+            cout << "_|";
+            //esto es lo que se imprime de nuestro tablero
+
+        }
+        
+        cout << endl;
+        //esto es para dar saltos de linea y asi contruya nuestro "tablero"
+    }
+
+}
+
+int main(){
+
+    construirTablero();
+    
+    return 0;
+}
